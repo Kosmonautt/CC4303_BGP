5e2db3573eb5a31bdbd7b3a832331b88b0a19fb9
Kosmonautt <b.contreras.2@ug.uchile.cl>
2023-11-22 04:08:35 -0300

    should be working

--- test_input.py
+++ test_input.py
@@ -2,12 +2,15 @@ import socket
 import sys
 import aux_functions
 
-# HASTA "nos" funciona, después falla
+# el port del que va 
+port_i = int(sys.argv[1])
+# el port al que va
+port_d = int(sys.argv[2])
 
 # el mensaje a enviar
 mssg = "hola!"
 # estructura
-struct = ["127.0.0.1", 8885, 10, 50, 0, len(mssg.encode()), 0, mssg]
+struct = ["127.0.0.1", port_d, 10, 50, 0, len(mssg.encode()), 0, mssg]
 # se pasa a mensaje
 full_mssg = aux_functions.create_packet(struct)
 # se pasa a bytes
@@ -20,4 +23,4 @@ print("full_mssg", full_mssg)
 # se crea el socket
 test_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # se envía el mensaje al socket en el puerto 8001
-test_socket.sendto(full_mssg, ("127.0.0.1", 8881))
+test_socket.sendto(full_mssg, ("127.0.0.1", port_i))
